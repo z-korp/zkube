6b07fb838b05ff172c1e6e369ef4fd970e91ec78
Cosmos 612 <cosmos.dev612@gmail.com>
2024-07-03 16:28:47 +0200

    Theme for tiki

--- client/src/ui/containers/GameBonus.tsx
+++ client/src/ui/containers/GameBonus.tsx
@@ -4,11 +4,11 @@ import imageHammer from "/assets/bonus/hammer.png";
 import imageTiki from "/assets/bonus/tiki.png";
 import imageWave from "/assets/bonus/wave.png";
 
-const handleClick = () => {
-  console.log("Ready to be binded");
-};
-
 export const GameBonus = () => {
+  const handleClick = () => {
+    console.log("Ready to be binded");
+  };
+
   return (
     <div className="grid grid-cols-3 gap-12 md:w-1/2 sm:w-2/3 bg-color">
       <div className="flex flex-col items-start">
--- client/src/ui/screens/Home.tsx
+++ client/src/ui/screens/Home.tsx
@@ -6,13 +6,15 @@ import BackGroundBoard from "../components/BackgroundBoard";
 import { AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
 
-import imageBackground from "/assets/theme-2.png";
-import imageBackgroundTotem from "/assets/theme-2-totem-2.png";
+import imageBackground from "/assets/theme-2-1.png";
+import imageTotemDark from "/assets/theme-2-totem-dark.png";
+import imageTotemLight from "/assets/theme-2-totem-light.png";
 import palmLeft from "/assets/palmtree-left.png";
 import palmRight from "/assets/palmtree-right.png";
 import PalmTree from "../components/PalmTree";
 import { Game } from "@/dojo/game/models/game";
 import { GameBonus } from "../containers/GameBonus";
+import { useTheme } from "../elements/theme-provider";
 
 export const Home = () => {
   const [animationDone, setAnimationDone] = useState(false);
@@ -29,6 +31,9 @@ export const Home = () => {
     [1, 0, 0, 0, 2, 2, 0, 0],
   ];
 
+  const { theme } = useTheme();
+  const imageTotemTheme = theme === "dark" ? imageTotemDark : imageTotemLight;
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setAnimationDone(true);
@@ -43,7 +48,7 @@ export const Home = () => {
 
       <BackGroundBoard imageBackground={imageBackground}>
         <BackGroundBoard
-          imageBackground={imageBackgroundTotem}
+          imageBackground={imageTotemTheme}
           initial={{ scale: 1 }}
           animate={{ scale: [1, 0.995, 1] }}
           transition={{
