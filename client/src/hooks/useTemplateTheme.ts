1ec380569326bb2e64e21c528a392f4ec2aef582
Cosmos 612 <cosmos.dev612@gmail.com>
2024-07-29 12:08:43 +0200

    Theme-1

--- client/src/hooks/useTemplateTheme.ts
+++ client/src/hooks/useTemplateTheme.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const useTemplateTheme = () => {
-  const [themeTemplate, setThemeTemplate] = useState("theme-2");
+  const [themeTemplate, setThemeTemplate] = useState("theme-1");
 
   return { themeTemplate };
 };
