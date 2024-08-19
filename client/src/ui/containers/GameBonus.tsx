1aee1c58f4271956846a49f783ab6e271be3209a
Matthias <matthias.monnier@gmail.com>
2024-08-19 10:20:26 -0400

    refacto

--- client/src/ui/containers/GameBonus.tsx
+++ client/src/ui/containers/GameBonus.tsx
@@ -18,7 +18,7 @@ export const GameBonus: React.FC<GameBonusProps> = ({
   onBonusHammerClick,
   hammerCount,
   tikiCount,
-  waveCount
+  waveCount,
 }) => {
   const { themeTemplate } = useTheme();
   const imgAssets = ImageAssets(themeTemplate);
@@ -39,15 +39,27 @@ export const GameBonus: React.FC<GameBonusProps> = ({
   };
 
   return (
-    <div className="grid grid-cols-3 gap-2 px-4 pb-2">
+    <div className="grid grid-cols-3 gap-3">
       <div className="flex flex-col items-start">
-        <BonusButton onClick={handleClickHammer} urlImage={imgAssets.hammer} bonusCount={hammerCount}/>
+        <BonusButton
+          onClick={handleClickHammer}
+          urlImage={imgAssets.hammer}
+          bonusCount={hammerCount}
+        />
       </div>
       <div className="flex flex-col items-center">
-        <BonusButton onClick={handleClickTiki} urlImage={imgAssets.tiki} bonusCount={tikiCount}/>
+        <BonusButton
+          onClick={handleClickTiki}
+          urlImage={imgAssets.tiki}
+          bonusCount={tikiCount}
+        />
       </div>
       <div className="flex flex-col w-full items-end">
-        <BonusButton onClick={handleClickWave} urlImage={imgAssets.wave} bonusCount={waveCount} />
+        <BonusButton
+          onClick={handleClickWave}
+          urlImage={imgAssets.wave}
+          bonusCount={waveCount}
+        />
       </div>
     </div>
   );
