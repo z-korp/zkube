77ca0119be6ab197a458ecfb4eca0341be3df9b4
Cheelax <thomas.belloc@gmail.com>
2024-08-23 09:30:55 -0400

    improve scale svg

--- client/src/ui/components/MaxComboIcon.tsx
+++ client/src/ui/components/MaxComboIcon.tsx
@@ -7,8 +7,8 @@ const MaxComboIcon = ({
 }) => (
   <svg
     className={className}
-    width={size}
-    height={size}
+    width="100%"
+    height="100%"
     viewBox="0 0 26 26"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
