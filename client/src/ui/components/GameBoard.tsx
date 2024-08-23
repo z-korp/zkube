a3f2ae290c8ec49cb1097b2de39353e8b197da1f
Cheelax <thomas.belloc@gmail.com>
2024-08-23 09:11:52 -0400

    .

--- client/src/ui/components/GameBoard.tsx
+++ client/src/ui/components/GameBoard.tsx
@@ -902,7 +902,7 @@ const GameBoard: React.FC<GameBoardProps> = ({
             className={`flex grow ${isMdOrLarger ? "text-4xl" : "text-2xl"} sm:gap-2 gap-[2px] justify-end relative ml-4`}
           >
             {maxCombo}
-            <MaxComboIcon className="text-slate-500 w-[26px] h-[36px] mt-auto" />
+            <MaxComboIcon className="text-slate-500 w-[26px] h-[42px] mt-auto" />
           </div>
         </div>
         <div className="bg-slate-800 relative">
