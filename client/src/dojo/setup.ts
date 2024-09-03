044496bab8aeb31438f5dbbdf43b1e56aef6c47a
Cristian Fleita <cristianagustinfleita@gmail.com>
2024-09-03 16:54:30 -0300

    chore: added queries to improve entities fetching

--- client/src/dojo/setup.ts
+++ client/src/dojo/setup.ts
@@ -1,4 +1,4 @@
-import { getSyncEntities } from "@dojoengine/state";
+import { getSyncEntities ,setEntities, syncEntities } from "@dojoengine/state";
 import * as torii from "@dojoengine/torii-client";
 import { models } from "./models.ts";
 import { systems } from "./systems.ts";
@@ -9,8 +9,10 @@ import { setupWorld } from "./contractSystems.ts";
 import { DojoProvider } from "@dojoengine/core";
 import { BurnerManager } from "@dojoengine/create-burner";
 import { Account, RpcProvider } from "starknet";
+import { Component, Metadata, Schema } from "@dojoengine/recs";
 
 export type SetupResult = Awaited<ReturnType<typeof setup>>;
+let sync: any;
 
 export async function setup({ ...config }: Config) {
   const toriiClient = await torii.createClient({
@@ -24,14 +26,29 @@ export async function setup({ ...config }: Config) {
 
   const clientModels = models({ contractModels });
 
-  // fetch all existing entities from torii
-  // await getSyncEntities(toriiClient, contractModels as any, []);
-  const sync = await getSyncEntities(
-    toriiClient,
-    contractModels as any,
-    [],
-    1000,
-  );
+  const getEntitiesQuery = async <S extends Schema>(
+    client: torii.ToriiClient,
+    components: Component<S, Metadata, undefined>[], query: torii.Query,
+    limit: number = 100, 
+    ) => {
+        let cursor = 0;
+        let continueFetching = true;
+        contractModels.Player
+
+        while (continueFetching) {
+            query.offset = cursor;
+            const fetchedEntities: torii.Entities = await client.getEntities(query);
+            console.log(fetchedEntities);
+
+            setEntities(fetchedEntities, components as any);
+
+            if (Object.keys(fetchedEntities).length < limit) {
+                continueFetching = false;
+            } else {
+                cursor += limit;
+            }
+        }
+    };
 
   const client = await setupWorld(
     new DojoProvider(config.manifest, config.rpcUrl),
@@ -65,6 +82,52 @@ export async function setup({ ...config }: Config) {
     console.error(e);
   }
 
+  // const componentArray = Object.values(contractModels);
+  // const names = [ ...componentArray.map((c) => c.metadata?.name as string),];
+  // console.log(names);
+
+  async function syncEntitiesForGameID() {
+    const burner = burnerManager.account?.address;
+  
+    const playerClause: torii.KeysClause = {
+      keys: [undefined],
+      pattern_matching: "FixedLen",
+      models: ["zkube-Player"]
+    };
+  
+    const gameClause: torii.MemberClause = {
+      model: "zkube-Game",
+      member: "player_id",
+      operator: "Eq",
+      value: {Felt252: burner}
+    };
+  
+    const PlayerQuery: torii.Query = {
+      limit: 10000,
+      offset: 0,
+      clause: {Keys: playerClause}
+    }; 
+  
+    const GameQuery: torii.Query = {
+      limit: 10000,
+      offset: 0,
+      clause: {Member: gameClause}
+    }; 
+  
+    await getEntitiesQuery(toriiClient, contractModels as any, PlayerQuery);
+    await getEntitiesQuery(toriiClient, contractModels as any, GameQuery);
+    sync = await syncEntities(toriiClient, contractModels as any, []);
+  
+    //fetch all existing entities from torii
+    // sync = await getSyncEntities(
+    //   toriiClient,
+    //   contractModels as any,
+    //   [],
+    //   1000,
+    // );
+
+  }
+
   return {
     client,
     clientModels,
@@ -76,5 +139,6 @@ export async function setup({ ...config }: Config) {
     rpcProvider,
     sync,
     toriiClient,
+    syncCallback: async () => await syncEntitiesForGameID()
   };
 }
