d4e7ec99663a955f675611720d1bef0f4d75129a
josephchimebuka <chimebujoseph@gmail.com>
2024-10-06 05:18:11 +0000

    added the burner opton for as a connector for account

--- client/src/hooks/useAccountCustom.tsx
+++ client/src/hooks/useAccountCustom.tsx
@@ -6,7 +6,7 @@ import { Account } from "starknet";
 type AccountType = "burner" | "controller";
 
 // eslint-disable-next-line prefer-const
-export let ACCOUNT_CONNECTOR: AccountType = "controller";
+export let ACCOUNT_CONNECTOR: AccountType = "burner";
 
 const useAccountCustom = () => {
   const { account } = useAccount();
