ef1eb847e566357dcb471025d08d96aadc300ac7
Cheelax <thomas.belloc@gmail.com>
2024-10-16 10:27:20 +0200

    add guard

--- client/src/utils/supabase.ts
+++ client/src/utils/supabase.ts
@@ -2,6 +2,11 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (import.meta.env.VITE_SEND_TO_SUPABASE && (!supabaseUrl || !supabaseKey)) {
+  throw new Error("Missing Supabase environment variables");
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
