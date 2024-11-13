97bec702fcca67812f56c1a634bd8e15864cf8f6
Cosmos 612 <cosmos.dev612@gmail.com>
2024-11-13 11:04:04 +0100

    blonk image

--- client/src/ui/components/HeaderNftBalance.tsx
+++ client/src/ui/components/HeaderNftBalance.tsx
@@ -1,5 +1,5 @@
 import useAccountCustom from "@/hooks/useAccountCustom";
-import React from "react";
+import React, { useMemo } from "react";
 import { useNftBalance } from "@/hooks/useNftBalance";
 import NftZKUBE from "/assets/nft-zkube.png";
 
@@ -7,13 +7,16 @@ const HeaderNftBalance = React.memo(() => {
   const { account } = useAccountCustom();
 
   const { balance } = useNftBalance(account?.address ? account.address : "");
+  const memoizedImage = useMemo(
+    () => <img src={NftZKUBE} alt="ZKUBE" className="ml-1 h-8 w-8" />,
+    [],
+  );
 
   if (account) {
     return (
       <span className="text-xs font-semibold md:font-normal flex items-center">
         {/*`${balance} Game${balance > 1 ? "s" : ""}`*/}
-        {`${balance}`}{" "}
-        <img src={NftZKUBE} alt="ZKUBE" className="ml-1 h-8 w-8" />
+        {`${balance}`} {memoizedImage}
       </span>
     );
   }
