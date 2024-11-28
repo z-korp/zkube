4ca5736254f88b4af3f1bfe4579ea7473c2171fe
Cosmos 612 <cosmos.dev612@gmail.com>
2024-11-28 16:37:20 +0100

    Clean imports 2

--- client/src/ui/components/Swipper.tsx
+++ client/src/ui/components/Swipper.tsx
@@ -1,16 +1,11 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperProps, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-cards";
 
 import "@/index.css";
 
-// import required modules
 import { EffectCards } from "swiper/modules";
-import GameModeCard from "./GameModeCard";
 import { ModeType } from "@/dojo/game/types/mode";
 import GameModeCardMobile from "./GameModeCardMobile";
 
