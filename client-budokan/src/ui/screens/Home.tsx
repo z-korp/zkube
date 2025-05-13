64beb76c4a397e5d1fe9a17e594105976d9f1c2c
Cheelax <thomas.belloc@gmail.com>
2025-05-13 18:57:15 +0200

    add back tutorial

--- client-budokan/src/ui/screens/Home.tsx
+++ client-budokan/src/ui/screens/Home.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/ui/containers/Header";
 import BackGroundBoard from "../components/BackgroundBoard";
 import { AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import ImageAssets from "@/ui/theme/ImageAssets";
 import PalmTree from "../components/PalmTree";
 import { useTheme } from "@/ui/elements/theme-provider/hooks";
@@ -16,10 +16,19 @@ import { useMediaQuery } from "react-responsive";
 import useViewport from "@/hooks/useViewport";
 import HeaderBalance from "../components/HeaderBalance";
 import { PlayFreeGame } from "../actions/PlayFreeGame";
+import { useGame } from "@/hooks/useGame";
+import Tutorial from "../components/Tutorial/Tutorial";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFire, faStar } from "@fortawesome/free-solid-svg-icons";
+import MaxComboIcon from "../components/MaxComboIcon";
+import GameBoard from "../components/GameBoard";
+import { useGrid } from "@/hooks/useGrid";
+import useAccountCustom from "@/hooks/useAccountCustom";
+import { Surrender } from "../actions/Surrender";
 
 export const Home = () => {
   useViewport();
-
+  const { account } = useAccountCustom();
   const [animationDone, setAnimationDone] = useState(false);
 
   const { theme, themeTemplate } = useTheme();
@@ -30,12 +39,47 @@ export const Home = () => {
   const imageTotemTheme =
     theme === "dark" ? imgAssets.imageTotemDark : imgAssets.imageTotemLight;
 
+  const { game } = useGame({
+    gameId: 0,
+    shouldLog: true,
+  });
+
+  const grid = useGrid({ gameId: game?.id ?? 0, shouldLog: true });
+
+  const gameGrid: React.RefObject<HTMLDivElement> | null = useRef(null);
+
+  const [isGameOn, setIsGameOn] = useState<"idle" | "isOn" | "isOver">("idle");
+
+  // State variables for modals
+  const [isTournamentsOpen, setIsTournamentsOpen] = useState(false);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setAnimationDone(true);
     }, 4000);
 
     return () => clearTimeout(timer);
+  }, []);
+
+  const [tutorialState, setTutorialState] = useState({
+    isActive: false,
+    showGrid: false,
+    showText: true,
+  });
+
+  const endTutorial = useCallback(() => {
+    setTutorialState((prev) => ({
+      ...prev,
+      isActive: false,
+    }));
+  }, []);
+
+  const startTutorial = useCallback(() => {
+    setTutorialState({
+      isActive: true,
+      showGrid: true,
+      showText: true,
+    });
   }, []);
 
   // Define render functions
@@ -78,7 +122,7 @@ export const Home = () => {
 
   return (
     <div className="h-screen-viewport flex flex-col w-full" id="portal-root">
-      <Header onStartTutorial={() => console.log("Start Tutorial")} />
+      <Header onStartTutorial={startTutorial} />
 
       {/* Content Area */}
       <div className="flex flex-col flex-1 relative">
@@ -96,7 +140,96 @@ export const Home = () => {
             }}
           >
             <div className="relative flex flex-col gap-4 sm:gap-8 flex-grow items-center justify-start overflow-auto h-full">
-              {isMdOrLarger ? renderDesktopView() : renderMobileView()}
+              <div className="flex flex-col items-center gap-4 sm:gap-8 w-full max-w-4xl mt-2 sm:mt-4 p-2 md:p-0 h-full">
+                {tutorialState.isActive ? (
+                  <Tutorial
+                    showGrid={tutorialState.showGrid}
+                    endTutorial={endTutorial}
+                  />
+                ) : (
+                  <>
+                    {(!game || (!!game && isGameOn === "isOver")) && (
+                      <>
+                        {isMdOrLarger
+                          ? renderDesktopView()
+                          : renderMobileView()}
+                      </>
+                    )}
+
+                    {!!game && isGameOn === "isOver" && !isTournamentsOpen && (
+                      <>
+                        <div className="flex flex-col gap-4 mt-4 md:mt-0">
+                          <div className="p-6 rounded-lg shadow-lg w-full h-full bg-gray-900 m-2">
+                            <p className="text-4xl text-center mb-2">
+                              Game Over
+                            </p>
+
+                            <div className="flex gap-4 justify-center items-center">
+                              <div className="grow text-4xl flex gap-2 justify-end">
+                                {game.score}
+                                <FontAwesomeIcon
+                                  icon={faStar}
+                                  className="text-yellow-500"
+                                />
+                              </div>
+                              <div className="grow text-4xl flex gap-2 justify-end">
+                                {game.combo}
+                                <FontAwesomeIcon
+                                  icon={faFire}
+                                  className="text-slate-700"
+                                />
+                              </div>
+                              <div className="grow text-4xl flex gap-2 justify-end">
+                                {game.max_combo}
+                                <MaxComboIcon
+                                  width={36}
+                                  height={36}
+                                  className="text-slate-700"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </>
+                    )}
+                    {!!game && isGameOn === "isOn" && (
+                      <div className="relative w-full">
+                        <div
+                          ref={gameGrid}
+                          className="flex flex-col items-center game-container"
+                        >
+                          <GameBoard
+                            // Check if game is over because otherwise we can display
+                            // previous game data on the board while the new game is starting
+                            // and torii indexing
+                            initialGrid={grid}
+                            nextLine={game.isOver() ? [] : game.next_row}
+                            score={game.isOver() ? 0 : game.score}
+                            combo={game.isOver() ? 0 : game.combo}
+                            maxCombo={game.isOver() ? 0 : game.max_combo}
+                            hammerCount={
+                              game.isOver() ? 0 : game.hammer - game.hammer_used
+                            }
+                            totemCount={
+                              game.isOver() ? 0 : game.totem - game.totem_used
+                            }
+                            waveCount={
+                              game.isOver() ? 0 : game.wave - game.wave_used
+                            }
+                            account={account}
+                            game={game}
+                          />
+                        </div>
+                        {isMdOrLarger && (
+                          <div className="mt-4 sm:mt-0 sm:absolute sm:right-0 sm:bottom-0 sm:mb-4 flex justify-center sm:justify-end w-full">
+                            <Surrender gameId={game.id} />
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </>
+                )}
+              </div>
             </div>
             <AnimatePresence>
               {!animationDone && (
