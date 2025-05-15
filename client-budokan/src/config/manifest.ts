003306aa991c9cb17e72d39d34494eb63e6b7c14
Matthias <matthias.monnier@gmail.com>
2025-05-15 12:31:10 +0200

    fix manifest

--- client-budokan/src/config/manifest.ts
+++ client-budokan/src/config/manifest.ts
@@ -1,20 +1,13 @@
-import local from "../../../contracts/manifest_sepolia.json";
 import slot from "../../../contracts/manifest_slot.json";
-import slotdev from "../../../contracts/manifest_sepolia.json";
 import sepolia from "../../../contracts/manifest_sepolia.json";
-import mainnet from "../../../contracts/manifest_sepolia.json";
-import sepoliadev1 from "../../../contracts/manifest_sepolia.json";
-import sepoliadev2 from "../../../contracts/manifest_sepolia.json";
+import mainnet from "../../../contracts/manifest_mainnet.json";
 
 const deployType = import.meta.env.VITE_PUBLIC_DEPLOY_TYPE;
 
 const manifests = {
   sepolia,
   mainnet,
-  sepoliadev1,
-  sepoliadev2,
   slot,
-  slotdev,
 };
 
 // Fallback to `local` if deployType is not a key in `manifests`
